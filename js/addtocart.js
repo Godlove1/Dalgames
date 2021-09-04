021a32f94bc665265976591988794c1d4999dfda
Godlove <ntangakalegodlove@gmail.com>
2021-09-04 12:29:03 +0100

    Add files via upload

---
+++ js/addtocart.js
@@ -0,0 +1,9 @@
+/*let cart = document.querySelectorAll(".tocart");
+let counter = document.getElementById("count");
+	var a = 1;
+let inCart = cart.forEach(cart => {
+	cart.addEventListener('click', function () {
+		cart.value = "fuck";
+	})
+})
+*/
