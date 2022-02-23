2ddedf60a4b851343a2d026a5108051212fd586e
Godlove <ntangakalegodlove@gmail.com>
2022-02-23 21:49:08 +0100

    Delete js directory

--- js/addtocart.js
+++
@@ -1,9 +0,0 @@
-/*let cart = document.querySelectorAll(".tocart");
-let counter = document.getElementById("count");
-	var a = 1;
-let inCart = cart.forEach(cart => {
-	cart.addEventListener('click', function () {
-		cart.value = "fuck";
-	})
-})
-*/
